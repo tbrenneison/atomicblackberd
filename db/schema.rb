2ab5c9c3d9350817eac925b3820cf251dfc79676
Tori Brenneison <tori.brenneison@gmail.com>
2018-05-06 17:09:54 -0400

    styling + add author and create/update time to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_06_090009) do
+ActiveRecord::Schema.define(version: 2018_05_06_204316) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_05_06_090009) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author", default: "Tori Brenneison"
   end
 
   create_table "users", force: :cascade do |t|
